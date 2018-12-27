696343a5a9f60137e4a4e7891ca0e969fe754c30
A Glazer <avigayilglazer@gmail.com>
2018-12-27 17:24:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,11 +32,12 @@ end
 def hit?(card_total)
   prompt_user
   get_user_input
-  if get_user_input = "h"
+  answer = get_user_input
+  if answer == "h"
     card_total += deal_card
     return card_total
     hit?(card_total)
-  elsif get_user_input = "s"
+  elsif answer = "s"
     return card_total 
   else
     return invalid_command
